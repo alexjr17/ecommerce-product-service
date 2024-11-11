c7f4eb8bfa3580d0eb7133120fe8ac533b44aaec
alexjr17 <alexjose.r.r@gmail.com>
2024-11-11 17:46:07 -0500

    config autenticación, prueba produccion

--- src/main/java/com/ecommerce/product_service/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/product_service/infrastructure/config/SecurityConfig.java
@@ -1,23 +1,23 @@
-package com.ecommerce.product_service.infrastructure.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-import java.beans.BeanProperty;
-import java.util.Arrays;
-
-@Configuration
-@EnableWebFluxSecurity
-public class SecurityConfig {
-
+//package com.ecommerce.product_service.infrastructure.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.reactive.CorsWebFilter;
+//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+//
+//import java.beans.BeanProperty;
+//import java.util.Arrays;
+//
+//@Configuration
+//@EnableWebFluxSecurity
+//public class SecurityConfig {
+//
 //    @Bean
 //    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
 //        http
@@ -32,23 +32,23 @@ public class SecurityConfig {
 //
 //        return http.build();
 //    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Arrays.asList("*"));
-        corsConfig.setMaxAge(3600L);
-        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        corsConfig.setAllowedHeaders(Arrays.asList("*"));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
-
-        return new CorsWebFilter(source);
-    }
-}
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public CorsWebFilter corsWebFilter() {
+//        CorsConfiguration corsConfig = new CorsConfiguration();
+//        corsConfig.setAllowedOrigins(Arrays.asList("*"));
+//        corsConfig.setMaxAge(3600L);
+//        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//        corsConfig.setAllowedHeaders(Arrays.asList("*"));
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", corsConfig);
+//
+//        return new CorsWebFilter(source);
+//    }
+//}
--- src/main/java/com/ecommerce/product_service/infrastructure/security/JwtAuthenticationFilter.java
+++ src/main/java/com/ecommerce/product_service/infrastructure/security/JwtAuthenticationFilter.java
@@ -1,52 +1,52 @@
-package com.ecommerce.product_service.infrastructure.security;
-
-import com.ecommerce.product_service.domain.port.in.AuthUseCase;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.ReactiveSecurityContextHolder;
-import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
-import reactor.core.publisher.Mono;
-
-import java.util.Collections;
-
-@Slf4j
-@Component
-@RequiredArgsConstructor
-public class JwtAuthenticationFilter implements WebFilter {
-    private final AuthUseCase authUseCase;
-
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        log.error("Validation error occurred: {}");
-
-        String path = exchange.getRequest().getPath().value();
-        if (path.startsWith("/api/auth")) {
-            return chain.filter(exchange);
-        }
-
-        String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            String token = authHeader.substring(7);
-            return authUseCase.validateToken(token)
-                    .flatMap(valid -> {
-                        System.out.println(valid);
-                        if (valid) {
-                            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
-                                    "user", null, Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))
-                            );
-                            return chain.filter(exchange)
-                                    .contextWrite(ReactiveSecurityContextHolder.withAuthentication(auth));
-                        }
-                        return chain.filter(exchange);
-                    });
-        }
-
-        return chain.filter(exchange);
-    }
-}
+//package com.ecommerce.product_service.infrastructure.security;
+//
+//import com.ecommerce.product_service.domain.port.in.AuthUseCase;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.server.ServerWebExchange;
+//import org.springframework.web.server.WebFilter;
+//import org.springframework.web.server.WebFilterChain;
+//import reactor.core.publisher.Mono;
+//
+//import java.util.Collections;
+//
+//@Slf4j
+//@Component
+//@RequiredArgsConstructor
+//public class JwtAuthenticationFilter implements WebFilter {
+//    private final AuthUseCase authUseCase;
+//
+//    @Override
+//    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+//        log.error("Validation error occurred: {}");
+//
+//        String path = exchange.getRequest().getPath().value();
+//        if (path.startsWith("/api/auth")) {
+//            return chain.filter(exchange);
+//        }
+//
+//        String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+//        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+//            String token = authHeader.substring(7);
+//            return authUseCase.validateToken(token)
+//                    .flatMap(valid -> {
+//                        System.out.println(valid);
+//                        if (valid) {
+//                            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+//                                    "user", null, Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))
+//                            );
+//                            return chain.filter(exchange)
+//                                    .contextWrite(ReactiveSecurityContextHolder.withAuthentication(auth));
+//                        }
+//                        return chain.filter(exchange);
+//                    });
+//        }
+//
+//        return chain.filter(exchange);
+//    }
+//}
